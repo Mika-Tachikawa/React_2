321c65dc4000b796039709ee32b8061f249e72a9
EC2 Default User <129802934+Mika-Tachikawa@users.noreply.github.com>
2023-09-16 01:02:37 +0000

    コードの記述ファイルを変更

--- src/App.jsx
+++ src/App.jsx
@@ -23,25 +23,38 @@
 //}
 
 //export default App;
-import { useState } from "react";
-import { ColoredMessage } from "./components/ColoredMessage";
+
+
+
+//import { ColoredMessage } from "./components/ColoredMessage";
+
+//export const App = () => {
+  //console.log("レンダリング");
+  //const [num,setNum] = useState(0);
+  
+  //const onClickButton = () => {
+    //setNum(num + 1);
+  //};
+  
+  //return (
+    //<>
+      //<h1 style={{ color: "red" }}>こんにちは</h1>
+      //<ColoredMessage />
+      //<ColoredMessage color="blue">お元気ですか？</ColoredMessage>
+      //<ColoredMessage color="pink">元気です!</ColoredMessage>
+      //<button onClick={onClickButton}>ボタン</button>
+      //<p>{num}</p>
+    //</>
+  //);
+//};
+
+
 
 export const App = () => {
-  console.log("レンダリング");
-  const [num,setNum] = useState(0);
-  
-  const onClickButton = () => {
-    setNum(num + 1);
-  };
-  
   return (
-    <>
-      <h1 style={{ color: "red" }}>こんにちは</h1>
-      <ColoredMessage />
-      <ColoredMessage color="blue">お元気ですか？</ColoredMessage>
-      <ColoredMessage color="pink">元気です!</ColoredMessage>
-      <button onClick={onClickButton}>ボタン</button>
-      <p>{num}</p>
-    </>
+    <div>
+      <p>CSS Modules</p>
+      <button>ボタン</button>
+    </div>
   );
 };

c5997a3bf3e3a0344cfeff9ee0005d184f87163b
EC2 Default User <129802934+Mika-Tachikawa@users.noreply.github.com>
2023-09-16 00:19:29 +0000

    Stateの定義

--- src/App.jsx
+++ src/App.jsx
@@ -23,12 +23,14 @@
 //}
 
 //export default App;
-
+import { useState } from "react";
 import { ColoredMessage } from "./components/ColoredMessage";
 
 export const App = () => {
+  const [num,setNum] = useState(0);
+  
   const onClickButton = () => {
-    alert();
+    setNum(num + 1);
   };
   
   return (
@@ -38,6 +40,7 @@ export const App = () => {
       <ColoredMessage color="blue">お元気ですか？</ColoredMessage>
       <ColoredMessage color="pink">元気です!</ColoredMessage>
       <button onClick={onClickButton}>ボタン</button>
+      <p>{num}</p>
     </>
   );
 };
